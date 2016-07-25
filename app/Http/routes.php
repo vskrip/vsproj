901d3452bac5d9c819c0fb35b13f5a8183d2b164
vskrip <vskrip33@gmail.com>
2016-07-24 19:59:35 -0500

    Creating menu with view pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,15 +2,53 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Routes File
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
+| Here is where you will register all of the routes in an application.
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/', function () {
+        return view('home');
+    })->middleware('guest');
+
+    Route::get('/categories', function () {
+        return view('categories');
+    })->middleware('guest');
+
+    Route::get('/projects', function () {
+        return view('projects');
+    })->middleware('guest');
+
+    Route::get('/contact', function () {
+        return view('contact');
+    })->middleware('guest');
+
+    Route::get('/about', function () {
+        return view('about');
+    })->middleware('guest');
+
+
+    // Route::get('/tasks', 'TaskController@index');
+    // Route::post('/task', 'TaskController@store');
+    // Route::delete('/task/{task}', 'TaskController@destroy');
+
+    // Route::auth();
+
 });
---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container"
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <h1>Landing Page of Web Catalog</h1>
+        </div>
+    </div>
+</div>
+@endsection
